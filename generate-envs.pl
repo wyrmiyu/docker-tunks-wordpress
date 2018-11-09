b6ec28bdf6efac415b0eadca4774a90ec7ae48bc
Pekka Wallendahl <wyrmiyu@gmail.com>
2018-11-09 04:12:29 +0200

    Add docker-compose and other stuff

--- generate-envs.pl
+++ generate-envs.pl
@@ -26,14 +26,14 @@ my ($version) = ($version_str =~ /wp_version\s=\s'([0-9\.]+?)'/);
 my $sha1sum = (split / /, qx(curl -Ls https://wordpress.org/latest.tar.gz | sha1sum))[0];
 print("WORDPRESS_VERSION=", $version, "\n");
 print("WORDPRESS_SHA1=", $sha1sum, "\n");
-print("MYSQL_ROOT_PASSWORD='", create_rand(64, \@chars), "'\n");
-print("WORDPRESS_TABLE_PREFIX='", create_rand(8, \@chars), "_'\n");
+print("MYSQL_ROOT_PASSWORD=", create_rand(32, \@chars), "\n");
+print("WORDPRESS_TABLE_PREFIX=", create_rand(8, \@chars), "_\n");
 print("WORDPRESS_DEBUG=FALSE\n");
 foreach my $env (@wp_secrets) {
-  print("$env='", create_rand(64, \@salt_chars), "'\n");
+  print("$env=", create_rand(64, \@salt_chars), "\n");
 }
 print("#\n# CHANGE FOLLOWING TO MATCH YOUR SETUP!\n#\n");
-print("SVCUSER=nginx\n");
-print("SVCUID=1234\n");
-print("SVCGROUP=nginx\n");
-print("SVCGID=1234\n");
+print("EXTERNAL_HOST_IP=127.0.0.1\n");
+print("EXTERNAL_HOST_PORT=8080\n");
+print("SVCUSER=www-data\n");
+print("SVCGROUP=www-data\n");
